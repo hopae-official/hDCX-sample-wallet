db80e18aa6ac9db581eee0d343ad945354247adc
dvlprsh <dvlprsh103@gmail.com>
2025-06-12 21:08:57 +0900

    fix: claim detail

--- app/Issue/CredentialDetail.tsx
+++ app/Issue/CredentialDetail.tsx
@@ -1,4 +1,4 @@
-import { router, Stack, useLocalSearchParams } from 'expo-router';
+import { router, Stack, useLocalSearchParams } from "expo-router";
 import {
   StyleSheet,
   Text,
@@ -6,12 +6,12 @@ import {
   TouchableOpacity,
   ImageBackground,
   ScrollView,
-} from 'react-native';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { Card } from '@/components/ui/card';
-import { CredentialInfoMap, CredentialType } from '@/types';
-import { getCredentialClaims } from '@/utils';
-import { Colors } from '@/constants/Colors';
+} from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Card } from "@/components/ui/card";
+import { CredentialInfoMap, CredentialType } from "@/types";
+import { getCredentialClaims } from "@/utils";
+import { Colors } from "@/constants/Colors";
 
 export default function CredentialDetailScreen() {
   const params = useLocalSearchParams<{
@@ -29,7 +29,7 @@ export default function CredentialDetailScreen() {
     <>
       <Stack.Screen
         options={{
-          title: '',
+          title: "",
           headerLeft: () => (
             <TouchableOpacity onPress={() => router.back()}>
               <Ionicons name="chevron-back" size={27} />
@@ -41,12 +41,12 @@ export default function CredentialDetailScreen() {
       <ScrollView contentContainerStyle={styles.container} bounces={false}>
         <Card style={styles.credentialCard}>
           <ImageBackground
-            source={require('@/assets/images/card_bg.jpg')}
+            source={require("@/assets/images/card_bg.jpg")}
             style={styles.contentContainer}
           >
             <View style={styles.cardContent}>
               <View style={styles.circleImage}>
-                <Ionicons name="school-outline" size={24} color={'gray'} />
+                <Ionicons name="wallet-outline" size={24} color={"gray"} />
               </View>
               <Text style={styles.cardText}>
                 {CredentialInfoMap[credentialType]?.label}
@@ -59,7 +59,7 @@ export default function CredentialDetailScreen() {
           <Card style={styles.dataInfoCard}>
             <View style={styles.cardHeader}>
               <View style={styles.infoCircleImage}>
-                <Ionicons name="newspaper" size={15} color={'gray'} />
+                <Ionicons name="newspaper" size={15} color={"gray"} />
               </View>
               <View style={{ flex: 1 }}>
                 <Text style={styles.boldText}>Information</Text>
@@ -67,14 +67,20 @@ export default function CredentialDetailScreen() {
             </View>
 
             <Card style={styles.infoWrapper}>
-              {Object.entries(claims).map(([key, value]) => (
-                <View key={key}>
-                  <Text style={styles.infoLabelText}>
-                    {key.replace(/_/g, ' ').toUpperCase()}
-                  </Text>
-                  <Text style={styles.infoText}>{value}</Text>
-                </View>
-              ))}
+              {Object.entries(claims)
+                .filter(([_, value]) => {
+                  if (typeof value !== "string") return false;
+
+                  return !value.startsWith("data:image");
+                })
+                .map(([key, value]) => (
+                  <View key={key}>
+                    <Text style={styles.infoLabelText}>
+                      {key.replace(/_/g, " ").toUpperCase()}
+                    </Text>
+                    <Text style={styles.infoText}>{value}</Text>
+                  </View>
+                ))}
             </Card>
           </Card>
         </View>
@@ -85,39 +91,39 @@ export default function CredentialDetailScreen() {
 const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     backgroundColor: Colors.light.background,
     paddingTop: 20,
     paddingBottom: 40,
   },
   credentialCard: {
     //marginTop: 20,
-    width: '90%',
+    width: "90%",
     aspectRatio: 1.5,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: 'black',
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.25,
-    overflow: 'hidden',
+    overflow: "hidden",
   },
   contentContainer: {
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
+    backgroundColor: "transparent",
+    flexDirection: "row",
     flex: 1,
-    width: '100%',
+    width: "100%",
   },
   cardContent: {
     padding: 10,
     flex: 1,
   },
   cardText: {
-    color: 'white',
+    color: "white",
     fontSize: 18,
-    position: 'absolute',
+    position: "absolute",
     bottom: 15,
     right: 15,
   },
@@ -125,56 +131,56 @@ const styles = StyleSheet.create({
     width: 50,
     height: 50,
     borderRadius: 50 / 2,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderWidth: 1,
-    borderColor: 'lightgray',
-    backgroundColor: 'white',
+    borderColor: "lightgray",
+    backgroundColor: "white",
     margin: 3,
   },
   infoCircleImage: {
     width: 35,
     height: 35,
     borderRadius: 35 / 2,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     borderWidth: 1,
-    borderColor: 'lightgray',
-    backgroundColor: 'white',
+    borderColor: "lightgray",
+    backgroundColor: "white",
     marginRight: 4,
   },
   dataInfoContainer: {
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
     flex: 1,
   },
   dataInfoCard: {
     marginTop: 15,
-    width: '90%',
-    alignItems: 'center',
+    width: "90%",
+    alignItems: "center",
     padding: 10,
     backgroundColor: Colors.light.lightYellow,
-    borderColor: 'transparent',
+    borderColor: "transparent",
   },
   decsText: {
-    color: 'green',
+    color: "green",
   },
   cardHeader: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 10,
   },
   infoWrapper: {
     padding: 10,
     borderRadius: 5,
-    width: '100%',
+    width: "100%",
     gap: 15,
     backgroundColor: Colors.light.background,
-    borderColor: 'transparent',
+    borderColor: "transparent",
   },
   boldText: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   infoLabelText: {
     fontSize: 15,
